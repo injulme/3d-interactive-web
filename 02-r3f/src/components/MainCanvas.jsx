b26fb87c46e8b754a4802b57449f600af770cf5a
injulme <injulme0309@naver.com>
2024-02-19 18:03:22 +0900

    r3f 실습

---
+++ 02-r3f/src/components/MainCanvas.jsx
@@ -0,0 +1,38 @@
+import { Canvas } from '@react-three/fiber';
+import { Color } from 'three';
+import { Meshes } from './Meshes';
+import { Lights } from './Lights';
+import * as THREE from 'three';
+import { Controls } from './Controls';
+import { GLBModel } from './GLBModel';
+import { Dancer } from './Dancer';
+import { PostProcessor } from './PostProcessor';
+import { Physics } from '@react-three/cannon';
+
+export const MainCanvas = () => {
+  return (
+    <Canvas
+      gl={{ antialias: true }}
+      shadows={'soft'}
+      // shadows={{ enabled: true, type: THREE.PCFSoftShadowMap }}
+      // shadows={true}
+      camera={{
+        fov: 60,
+        aspect: window.innerWidth / window.innerHeight,
+        near: 0.1,
+        far: 100,
+        position: [5, 5, 5],
+      }}
+      scene={{ background: new Color(0x000000) }}
+    >
+      <Physics gravity={[0, -9, 0]} defaultContactMaterial={{ restitution: 0.1, friction: 1 }}>
+        <Lights />
+        <Meshes />
+      </Physics>
+      <Controls />
+      {/* <GLBModel /> */}
+      {/* <PostProcessor /> */}
+      {/* <Dancer /> */}
+    </Canvas>
+  );
+};
---
+++ 02-r3f/src/components/Meshes.jsx
@@ -0,0 +1,78 @@
+import { useBox, useSphere } from '@react-three/cannon';
+import { Box, Sphere } from '@react-three/drei';
+import { useEffect } from 'react';
+
+export const Meshes = () => {
+  const [planeRef] = useBox(() => ({
+    args: [50, 1, 50],
+    type: 'Static',
+    mass: 1,
+    position: [0, 0, 0],
+    material: {
+      restitution: 1,
+      friction: 0.5,
+    },
+    onCollide: () => {
+      console.log('collide!');
+    },
+  }));
+
+  const [boxRef, api] = useBox(() => ({
+    args: [1, 1, 1],
+    mass: 1,
+    position: [-1, 2, 0],
+    material: {
+      restitution: 0.4,
+      friction: 0.2,
+    },
+  }));
+
+  const [sphereRef1, sphereApi] = useSphere(() => ({
+    mass: 5,
+    position: [0.5, 8, 0],
+    material: {
+      restitution: 0.4,
+      friction: 0.1,
+    },
+  }));
+  const [sphereRef2] = useSphere(() => ({
+    mass: 0.2,
+    position: [1, 5, 0],
+    material: {
+      restitution: 0.2,
+      friction: 0.1,
+    },
+  }));
+
+  useEffect(() => {
+    api.applyForce([555, 50, 0], [1, 0, 0]);
+    sphereApi.applyLocalForce([-2000, 0, 0], [1, 0, 0]);
+  }, [api, sphereApi]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      api.applyLocalImpulse([0, 20, 0], [1, 0, 0]);
+      sphereApi.applyImpulse([200, 10, 0], [0, 0, 0]);
+    }, 3000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [api, sphereApi]);
+
+  return (
+    <>
+      <Box ref={planeRef} args={[50, 1, 50]}>
+        <meshStandardMaterial color={0xfefefe} roughness={0.3} metalness={0.8} />
+      </Box>
+      <Box ref={boxRef} args={[1, 1, 1]}>
+        <meshStandardMaterial color={0xff0000} roughness={0.3} metalness={0.8} />
+      </Box>
+      <Sphere ref={sphereRef1}>
+        <meshStandardMaterial color={0x9000ff} roughness={0.3} metalness={0.8} />
+      </Sphere>
+      <Sphere ref={sphereRef2}>
+        <meshStandardMaterial color={0xff00ff} roughness={0.3} metalness={0.8} />
+      </Sphere>
+    </>
+  );
+};
